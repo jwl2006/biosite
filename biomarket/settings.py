fd130945436ffde19fd132d36a6d7dde05fb4ac0
jwl2006 <wanghao313@gmail.com>
2016-02-03 15:38:54 -0800

    html changed

--- biomarket/settings.py
+++ biomarket/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development ssettings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
